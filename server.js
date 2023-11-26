42a948e52c4c5400999bf775562034b4e717d1b9
Ployphuk <ployphuk.jafarx@mail.kmutt.ac.th>
2023-11-26 10:30:35 +0700

    Add : database and insertdata

---
+++ server.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+const hostname = 'localhost';
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(express.static('public'));
+
+
+const db= mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'projectgamedb'
+});
+
+db.connect((err) => {
+    if (err) {
+        console.error('MySQL connection error:', err);
+    } else {
+        console.log('Connected to MySQL');
+    }
+});
+
+let tablename ="userdata";
+
+const queryDB = (sql) => {
+    return new Promise ((resolve,reject) => {
+        db.query(sql, (err,result, fields) =>{
+            if (err) reject(err);
+            else
+                resolve(result)
+        })
+    })
+}
+
+app.post("/register", async (req,res) => {
+    let sql = "CREATE TABLE IF NOT EXISTS userdata(id INT AUTO_INCREMENT PRIMARY KEY,reg_date TIMESTAMP, username VARCHAR(50), firstname VARCHAR(50), email VARCHAR(50), password VARCHAR(50), confirmpassword VARCHAR(50))";
+    let result = await queryDB(sql);
+    sql = `INSERT INTO userdata (username, firstname, email, password, confirmpassword) VALUES("${req.body.username}", "${req.body.firstname}","${req.body.email}","${req.body.password}","${req.body.confirmpassword}") `;
+    result = await queryDB(sql);
+    console.log("New Record successful");
+    return res.redirect("Login.html");
+})
+
+
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
